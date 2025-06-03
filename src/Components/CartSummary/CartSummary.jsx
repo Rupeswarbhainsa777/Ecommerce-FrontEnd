0cf622d3f77c782741d7c792065fd4708e57525b
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2025-06-04 02:59:47 +0530

    eleven

---
+++ src/Components/CartSummary/CartSummary.jsx
@@ -0,0 +1,9 @@
+import './CartSummary.css'
+
+const  CartSummary = () => {
+    return (
+        <div className="cartSummary">Hii fcgj</div>
+    )
+}
+
+export default CartSummary;
---
+++ src/Components/CustomerForm/CustomerForm.jsx
@@ -0,0 +1,7 @@
+import './CustomerForm.css'
+const CustomerForm = () => {
+    return (
+        <div className="CustomerForm">hhi from cost</div>
+    )
+}
+export default CustomerForm
